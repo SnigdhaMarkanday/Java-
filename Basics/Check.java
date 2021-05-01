62c46f597af359e730b5bb003ad1858490477ffb
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-05-01 15:55:51 +0530

    To check whether the n. entered by the user is int or not

---
+++ Basics/Check.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Check {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your number");
+		Scanner s = new Scanner(System.in);
+		System.out.println(s.hasNextInt());
+	
+	}
+}
