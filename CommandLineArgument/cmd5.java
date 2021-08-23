0eff17c6c4542b4f61102dbef708ec4ed6a66ef9
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 14:29:31 +0530

    Command Line Argument Example 5 (Swapping of 2 numbers using 3rd variable)

---
+++ CommandLineArgument/cmd5.java
@@ -0,0 +1,14 @@
+class Demo6{
+	public static void main(String []z){
+		int a,b,c;
+		a = Integer.parseInt(z[0]);
+		b = Integer.parseInt(z[1]);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+
+		
+	}
+}
