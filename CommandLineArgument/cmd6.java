211265e738010af3d61f9eda4aa5f089c1db074a
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 15:01:10 +0530

    Command Line Argument Example 6 (Swap without using 3rd variable)

---
+++ CommandLineArgument/cmd6.java
@@ -0,0 +1,14 @@
+class Demo6{
+	public static void main(String []z){
+		int a,b;
+		a = Integer.parseInt(z[0]);
+		b = Integer.parseInt(z[1]);
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println(a);
+		System.out.println(b);
+
+		
+	}
+}
