859a54163067559e53315c02f0900be01bdd036b
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 11:59:58 +0530

    Java basic concept3

---
+++ Basics/javaconcept3.java
@@ -0,0 +1,8 @@
+class Demo4{
+	public static void main(String []args){
+	System.out.println("Hello");
+	System.out.println("Java");//for printing in next line 
+	System.out.print("Hello"); // for printing in same line 
+	System.out.print("Java");
+	}
+}
