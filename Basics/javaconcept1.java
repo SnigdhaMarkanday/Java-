552f34e274c97e2375b0b72cc4858b64eb228018
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 10:54:57 +0530

    java Concept2

---
+++ Basics/javaconcept1.java
@@ -0,0 +1,16 @@
+class one{
+	
+}
+class Two{
+	
+}
+class Three{
+	public static void main(String []args){
+		System.out.println("Only this main class will run if java Three command is used");
+	}
+}
+class Demo2{
+	public static void main(String []args){
+	System.out.println("Only and only Demo2 main class will run when java Demo2 command is used");
+	}
+}
