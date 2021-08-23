108ffb96189b6d38d63df8dc3c27967788e5e5bc
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 11:46:53 +0530

    Understanding of Java Concept3

---
+++ Basics/javaconcept2.java
@@ -0,0 +1,14 @@
+class one{
+	
+}
+class Two{
+	
+}
+public class Three{
+	
+}
+class Demo3{
+	public static void main(String []args){
+	System.out.println("In this case main class won't be accessed as we have made class Three as public and increased its visibility");
+	}
+}// error as public class is different and main class is different
