03cbb0bc8e2ff646138e54745994a6af07bc31fc
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-23 09:52:09 +0530

    Java important Concept

---
+++ Basics/javaconcept.java
@@ -0,0 +1,14 @@
+class one{
+	
+}
+class Two{
+	
+}
+class Three{
+	
+}
+class Demo1{
+	public static void main(String []args){
+		System.out.println("for compilation we can select any name for java program but for running we have to use the main class name only);
+	}
+}
