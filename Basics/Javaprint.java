dd06b594d45ebdb493fdf6a295433520d476f541
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-25 11:07:15 +0530

    A program for printing Java pattern

---
+++ Basics/Javaprint.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+    System.out.println("\t   J\t  a \t v\tv\t  a");
+    System.out.println("\t   J\t a a \t  v    v\t a a ");
+    System.out.println("\tJ  J\taaaaa \t   v  v \taaaaa");
+    System.out.println("\t JJ\ta    a \t    v\t\ta    a");
+	}
+}
