933c3eec6d514a3a393e0e5f8f666d7ddd87b71b
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-25 11:14:17 +0530

    Write a program to compute specified Formula

---
+++ Basics/Compute.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+    double a,b,d;
+    a = (25.5*3.5-3.5*3.5);
+    b = (40.5-4.5);
+    d = a/b;
+    System.out.println(d);
+	}
+}
