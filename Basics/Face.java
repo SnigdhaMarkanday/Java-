b68515990c0d30bf365e708e1e12804050a60b26
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-26 08:59:54 +0530

    Write a Java program to print a face

---
+++ Basics/Face.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] z)throws IOException{
+		System.out.println("+ \" \" \" \" \" +");
+		System.out.println("[ | o   o | ]");
+		System.out.println("  |   ^   |");
+		System.out.println("  | ' _ ' | ");
+		System.out.println("+_ _ _ _ _ +");
+		
+		
+        
+		
+	}
+}
