78e9492a0700f3c94bf9194156c914a78726580c
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-26 05:51:10 +0530

    Write a java program that takes two numbers as an input and displays the product of the two numbers

---
+++ Basics/product.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] z)throws IOException{
+		InputStreamReader a = new InputStreamReader (System.in);
+		BufferedReader b = new BufferedReader(a);
+		int n1,n2,p;
+		System.out.println("Enter First Number");
+		n1 = Integer.parseInt(b.readLine());
+		System.out.println("Enter Second Number");
+		n2 = Integer.parseInt(b.readLine());
+		p = n1*n2;
