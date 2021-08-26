e3962f7582cc56e668d8e3b2b09230c052d736d6
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-26 08:06:37 +0530

    Write a java program to compute the specified expressions and print the output

---
+++ Basics/Computeoutput.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] z)throws IOException{
+		double a,b,d;
+		a = (25.5*3.5 -3.5*3.5);
+		b = (40.5-4.5);
+		d = a/b;
+        System.out.println(d);
+        
+		
+	}
+}
