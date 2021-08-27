2ce30b75f0d20db8c2fe647b6128f62a611a84ba
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-27 19:17:18 +0530

    Update String Array.java

--- Strings/String Array.java
+++ Strings/String Array.java
@@ -1,3 +1,79 @@
 import java.io.*;
-public class main(){
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    InputStreamReader a = new InputStreamReader(System.in);
+	    BufferedReader b = new BufferedReader(a);
+	    int m;
+	    System.out.println("Choose From Menu \n 1.Create \n2.Display\n3.Swap\n4.Reverse List of Names\n 5.To Upper Case\n 6.To LowerCase 7.Title\n8.Toggle Case\n 9.Reverse Names\n 10.Sort List\n 11.Sort Names\n 12.Search Names\n13.Replace Locally\n14.Replace Globally \n 15.Replace String\n 16.Join and Create\n 17.display new String\n 18. Split in new string array \n19.Pallindrome\n 20.Display new String Array\n 21.Exit\n");
+	    m = Integer.parseInt(m);
+	    Switch(m){
+	        case 1: 
+	            //Create
+	            break;
+	       case 2: 
+	           //Display 
+	           break;
+	       case 3:
+	           //Swap
+	           break;
+	       case 4:
+	           //Reverse
+	           break;
+	       case 5:
+	           //change to upper case 
+	           break;
+	       case 6:
+	           //change to lower case
+	           break;
+	       case 7:
+	           //Title case
+	           break;
+	       case 8:
+	           //Toogle case 
+	           break;
+	       case 9:
+	           //Reverse Names
+	           break;
+	       case 10:
+	           //Sort List
+	           break;
+	       case 11:
+	           //Sort Names
+	           break;
+	       case 12:
+	           //Search Names
+	           break;
+	       case 13:
+	           //Replace Locally 
+	            break;
+	       case 14:
+	           //Replace Globally 
+	           break;
+	       case 15:
+	           //Replace String
+	           break;
+	       case 16:
+	           //Join and Create
+	           break;
+	       case 17:
+	           //Display new string array 
+	           break;
+	       case 18:
+	           //Split in new String array
+	           break;
+	       case 19:
+	           //Pallindrome
+	           break;
+	       case 20:
+	           //Display new String array
+	           break;
+	       case 21:
+	           //Exit
+	           break;
+	  
+	    }
+		
+	}
+}
   
