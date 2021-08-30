b5c48f8ec07c31d81f7c1df5fafb9af096625cbd
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-30 10:45:14 +0530

    Pass a number from cmd and display the sum of smallest number and number itself

---
+++ CommandLineArgument/Example 6.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] z) {
+	    int n,s;
+	    n =Integer.parseInt(z[[0]);
+	    s = (n*(n+1))/2;
+	    System.out.println("Sum of the numbers is "+s);
+	}
+}
