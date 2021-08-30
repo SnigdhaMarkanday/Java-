39db77042812010696199a582d9240735d519ff6
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-08-30 14:36:47 +0530

    Input an array of five elements and print its reverse

---
+++ Arrays/Reverse.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] z) throws IOException{
+	   InputStreamReader a = new InputStreamReader(System.in);
+	   BufferedReader b = new BufferedReader(a);
+	   int n[] = new int[5];
+	   int i;
+	   for(i=0;i<=4;i++){
+	       System.out.println("Enter Element");
+	       n[i] =Integer.parseInt(b.readLine());
+	       
+	   }
+	   for(i=4;i>=0;i--){
+	       System.out.println(n[i]);
+	   }
+	   
+	 }
+}
