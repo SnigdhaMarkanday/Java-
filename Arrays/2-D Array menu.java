a538800ae30291429456d8344e2efd7facf2205c
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-09-07 14:36:21 +0530

    Updating 2D Array Menu

--- Arrays/2-D Array menu.java
+++ Arrays/2-D Array menu.java
@@ -7,7 +7,10 @@ class Sum{
 			boolean k;
 			int n[][] = new int [3][3];
 			int t[][] = new int [3][3];
-			int sr,sc,ld,rd,is,os;
+			int n1[][] = new int [3][3];
+			int a1[][] = new int [3][3];
+			int m1[][] = new int [3][3];
+			int sr,sc,ld,rd,is,os,c1,r1;
 			int r,c;
 				
 			k = true;
@@ -153,9 +156,108 @@ class Sum{
 						//Row Sort 
 						for(r=0;r<=2;r++){
 							for(c=0;c<=2;c++){
-								
+								for(c1=c+1;c1<=2;c1++){
+									if(n[r][c]>n[r][c1]){
+										int temp;
+										temp=n[r][c1];
+										n[r][c1]=n[r][c];
+										n[r][c]=temp;
+									}
+
+								}
+
 							}
 						}
+						for(r=0;r<=2;r++){
+							for(c=0;c<3;c++){
+								System.out.print(n[r][c]+"\t");
+							}
+							System.out.println();
+						}
+						break;
+						case 12:
+						//Column Sort 
+						for(r=0;r<=2;r++){
+							for(c=r+1;c<=2;c++){
+						
+									if(n[c][r]>n[r][c]){
+										int temp;
+										temp = n[r][c];
+										n[r][c] = n[c][r];
+										n[c][r]= temp;
+									}
+								
+
+							}
+						}
+						for(r=0;r<=2;r++){
+							for(c=0;c<3;c++){
+								System.out.print(n[r][c]+"\t");
+							}
+							System.out.println();
+						}
+						break;
+						case 13:
+						//
+						case 15:
+						//creating 2nd 2D Array
+							for(r=0;r<=2;r++){
+								for(c=0;c<=2;c++){
+										System.out.println("Enter Element ");
+										n1[r][c] = Integer.parseInt(b.readLine());
+								}
+							}
+						break;
+						case 16:
+						//Displaying 2nd 2D Array
+						for(r=0;r<=2;r++){
+								for(c=0;c<=2;c++){
+									System.out.print(n1[r][c]+ "\t");
+								}
+								System.out.println();
+							}
+							break;
+						case 17:
+						//Addition
+						for(r=0;r<=2;r++){
+							for(c=0;c<=2;c++){
+								a1[r][c]= n[r][c]+n1[r][c];
+							}
+						}
+						break;
+						case 18: 
+						//Displaying adiition matrix
+						for(r=0;r<=2;r++){
+								for(c=0;c<=2;c++){
+									System.out.print(a1[r][c]+ "\t");
+								}
+								System.out.println();
+							}
+						break;
+						case 19:
+						//Multiplication
+						for(r=0;r<=2;r++){
+							for(c=0;c<=2;c++){
+								int km1;
+								m1[r][c]=0;
+								for(km1=0;km1<=2;km1++){
+									m1[r][c] +=n[r][km1]*n1[km1][c];
+								}
+
+							}
+						}
+						break;
+						case 20:
+						//Displaying Multiplication matrix
+						for(r=0;r<=2;r++){
+								for(c=0;c<=2;c++){
+									System.out.print(m1[r][c]+ "\t");
+								}
+								System.out.println();
+							}
+						break;
+
+
 						case 21:
 						  k = false;
 						  break;
